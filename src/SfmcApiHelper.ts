59c2b3763491ac92cb514af60a2f8320ce1754b0
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-08-10 19:03:03 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -6,6 +6,14 @@ import Utils from './Utils';
 
 export default class SfmcApiHelper
 {
+    
+  private _accessToken = "";
+  private member_id = "514018007";
+  private soap_instance_url = "";
+  private FolderID = "";
+  private ParentFolderID = "";
+  private userName = "";
+
     // Instance variables
     private _deExternalKey = "DF18Demo";
     private _sfmcDataExtensionApiUrl = "https://mcj6cy1x9m-t5h5tz0bfsyqj38ky.rest.marketingcloudapis.com/hub/v1/dataevents/key:" + this._deExternalKey + "/rowset";
@@ -105,6 +113,182 @@ export default class SfmcApiHelper
             });
         });
     }
+    public creatingDomainConfigurationDE(
+    req: express.Request,
+    res: express.Response,
+    member_id: string,
+    soap_instance_url: string,
+    refreshToken: string,
+    FolderID: string,
+    tssd: string
+  ) {
+    //this.getRefreshTokenHelper(this._accessToken, res);
+    console.log("creatingDomainConfigurationDE:" + member_id);
+    console.log("creatingDomainConfigurationDE:" + soap_instance_url);
+    console.log("creatingDomainConfigurationDE:" + refreshToken);
+    Utils.logInfo("creatingDomainConfigurationDE:" + FolderID);
+    console.log("creatingDomainConfigurationDE:" + tssd);
+
+    //console.log('domainConfigurationDECheck:'+req.body.ParentFolderID);
+
+    let refreshTokenbody = "";
+    this.getRefreshTokenHelper(refreshToken, tssd, false, res)
+      .then((response) => {
+        Utils.logInfo(
+          "creatingDomainConfigurationDE:" +
+          JSON.stringify(response.refreshToken)
+        );
+        Utils.logInfo(
+          "creatingDomainConfigurationDE:" + JSON.stringify(response.oauthToken)
+        );
+        refreshTokenbody = response.refreshToken;
+        Utils.logInfo(
+          "creatingDomainConfigurationDE1:" + JSON.stringify(refreshTokenbody)
+        );
+
+        let DCmsg =
+          '<?xml version="1.0" encoding="UTF-8"?>' +
+          '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">' +
+          "    <s:Header>" +
+          '        <a:Action s:mustUnderstand="1">Create</a:Action>' +
+          '        <a:To s:mustUnderstand="1">' +
+          soap_instance_url +
+          "Service.asmx" +
+          "</a:To>" +
+          '        <fueloauth xmlns="http://exacttarget.com">' +
+          response.oauthToken +
+          "</fueloauth>" +
+          "    </s:Header>" +
+          '    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">' +
+          '        <CreateRequest xmlns="http://exacttarget.com/wsdl/partnerAPI">' +
+          '            <Objects xsi:type="DataExtension">' +
+          "                <CategoryID>" +
+          FolderID +
+          "</CategoryID>" +
+          "                <CustomerKey>Pashtek Developer-" +
+          member_id +
+          "</CustomerKey>" +
+          "                <Name>Pashtek Developer-" +
+          member_id +
+          "</Name>" +
+          "                <Fields>" +
+          "                    <Field>" +
+          "                        <CustomerKey>Name</CustomerKey>" +
+          "                        <Name>Name</Name>" +
+          "                        <FieldType>Text</FieldType>" +
+          "                        <MaxLength>50</MaxLength>" +
+          "                        <IsRequired>true</IsRequired>" +
+          "                        <IsPrimaryKey>false</IsPrimaryKey>" +
+          "                    </Field>" +
+          "                    <Field>" +
+          "                        <CustomerKey>Phone NUmber</CustomerKey>" +
+          "                        <Name>Phone Number</Name>" +
+          "                        <FieldType>Number</FieldType>" +
+          "                        <MaxLength>10</MaxLength>" +
+          "                        <IsRequired>true</IsRequired>" +
+          "                        <IsPrimaryKey>true</IsPrimaryKey>" +
+          "                    </Field>" +
+          "                    <Field>" +
+          "                        <CustomerKey>Position</CustomerKey>" +
+          "                        <Name>Position</Name>" +
+          "                        <FieldType>Text</FieldType>" +
+          "                        <MaxLength>20</MaxLength>" +
+          "                        <IsRequired>true</IsRequired>" +
+          "                        <IsPrimaryKey>false</IsPrimaryKey>" +
+          "                    </Field>" +
+          "                    <Field>" +
+          "                        <CustomerKey>Years of Experience</CustomerKey>" +
+          "                        <Name>Years of Experience</Name>" +
+          "                        <FieldType>Number</FieldType>" +
+          "                        <MaxLength>5</MaxLength>" +
+          "                        <IsRequired>true</IsRequired>" +
+          "                        <IsPrimaryKey>false</IsPrimaryKey>" +
+          "                    </Field>" +       
+          "                </Fields>" +
+          "            </Objects>" +
+          "        </CreateRequest>" +
+          "    </s:Body>" +
+          "</s:Envelope>";
+
+        return new Promise<any>((resolve, reject) => {
+          let headers = {
+            "Content-Type": "text/xml",
+          };
+
+          axios({
+            method: "post",
+            url: "" + soap_instance_url + "Service.asmx" + "",
+            data: DCmsg,
+            headers: headers,
+          })
+            .then((response: any) => {
+             
+                response.data,
+                (
+                  err: any,
+                  result: {
+                    [x: string]: {
+                      [x: string]: { [x: string]: { [x: string]: any }[] }[];
+                    };
+                  }
+                ) => {
+                  let DomainConfiguration =
+                    result["soap:Envelope"]["soap:Body"][0][
+                    "CreateResponse"
+                    ][0]["Results"];
+
+                  if (DomainConfiguration != undefined) {
+                    let DEexternalKeyDomainConfiguration =
+                      DomainConfiguration[0]["Object"][0]["CustomerKey"];
+
+                    //this.DEexternalKeyDomainConfiguration =
+                    // DomainConfiguration[0]["Object"][0]["CustomerKey"];
+                    let sendresponse = {};
+                    sendresponse = {
+                      refreshToken: refreshTokenbody,
+                      statusText:
+                        "Domain Configuration Data extension has been created Successfully",
+                      soap_instance_url: soap_instance_url,
+                      member_id: member_id,
+                      DEexternalKeyDomainConfiguration:
+                        DEexternalKeyDomainConfiguration,
+                    };
+                    res.status(200).send(sendresponse);
+
+                    /*  res
+                  .status(200)
+                  .send(
+                    "Domain Configuration Data extension has been created Successfully"
+                  );*/
+                  }
+                }
+              
+            })
+            .catch((error: any) => {
+              // error
+              let errorMsg =
+                "Error creating the Domain Configuration Data extension......";
+              errorMsg += "\nMessage: " + error.message;
+              errorMsg +=
+                "\nStatus: " + error.response
+                  ? error.response.status
+                  : "<None>";
+              errorMsg +=
+                "\nResponse data: " + error.response.data
+                  ? Utils.prettyPrintJson(JSON.stringify(error.response.data))
+                  : "<None>";
+              Utils.logError(errorMsg);
+
+              reject(errorMsg);
+            });
+        });
+      })
+      .catch((error: any) => {
+        res
+          .status(500)
+          .send(Utils.prettyPrintJson(JSON.stringify(error.response.data)));
+      });
+  }
 
     /**
      * loadData: called by the GET handlers for /apidemoloaddata and /appdemoloaddata
