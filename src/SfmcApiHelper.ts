9295101c04066257131ef05cc195c83a0be5c299
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-08-30 16:15:26 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -292,11 +292,11 @@ export default class SfmcApiHelper
               });
           });
         
-        .catch((error: any) => {
-          res
-            .status(500)
-            .send(Utils.prettyPrintJson(JSON.stringify(error.response.data)));
-        });
+        // .catch((error: any) => {
+        //   res
+        //     .status(500)
+        //     .send(Utils.prettyPrintJson(JSON.stringify(error.response.data)));
+        // });
     } 
     public creatingDomainConfigurationDE(
     req: express.Request,
