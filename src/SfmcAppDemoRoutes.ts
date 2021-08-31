c7454b61b2cab3bb8813b5abdcb6872644fddb1c
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-08-31 18:23:57 +0530

    Update SfmcAppDemoRoutes.ts

--- src/SfmcAppDemoRoutes.ts
+++ src/SfmcAppDemoRoutes.ts
@@ -151,6 +151,7 @@ export default class SfmcAppDemoRoutes
         res: express.Response,
         
       ) {
+        console.log("mkmkmkmkmk");
         
         let self = this;
        self._apiHelper.creatingDomainConfigurationDE(req, res);
