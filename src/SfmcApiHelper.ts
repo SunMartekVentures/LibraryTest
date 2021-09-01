b6d20a95d1ec224aad2a1c28c40c4ecc72e481ae
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-09-01 11:13:01 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -380,7 +380,7 @@ export default class SfmcApiHelper
             .then((response: any) => {
              
                console.log("response-",response);
-               
+               res.send(response);
               
             })
             .catch((error: any) => {
