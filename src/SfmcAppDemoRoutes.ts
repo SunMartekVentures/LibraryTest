007ab9d0270bafcc155e6aa34336c6c4313b7e77
Manikandanpashtek <86593624+Manikandanpashtek@users.noreply.github.com>
2021-09-16 16:49:31 +0530

    Update SfmcAppDemoRoutes.ts

--- src/SfmcAppDemoRoutes.ts
+++ src/SfmcAppDemoRoutes.ts
@@ -99,8 +99,10 @@ export default class SfmcAppDemoRoutes
       let sessionId = req.session.id;
       let clientId = process.env.DF18DEMO_CLIENTID;
       let clientSecret = process.env.DF18DEMO_CLIENTSECRET;
+      let code = req.body.authorization_code;
+
       let session = req.session;
-      console.log("clientId",clientId,"clientSecret",clientSecret);
+      console.log("clientId",clientId,"clientSecret",clientSecret,"code",code);
       
       // req.session.oauthAccessToken = "";
       // req.session.oauthAccessTokenExpiry = "";
