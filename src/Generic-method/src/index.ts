331ed3732c9ec75ff0c51649d0f770154a1289ad
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-10-04 12:29:58 +0530

    Update index.ts

--- src/Generic-method/src/index.ts
+++ src/Generic-method/src/index.ts
@@ -43,6 +43,7 @@ export default class mcGenericMethods {
       .then((response: any) => {
         // If the GET request is successful, this block is executed
         result = response; // The response of the API call is passed on to the next block
+        console.log("respo in lib oauth:",response);
       })
       .catch((err: any) => {
         result = "Error getting access token >>> ";
