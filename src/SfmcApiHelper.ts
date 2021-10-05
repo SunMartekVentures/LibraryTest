d6b3c9ff5b5efcf93dafcb691be8510e5d111f4f
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-10-05 14:00:20 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -148,19 +148,16 @@ export default class SfmcApiHelper
                  parentFolderId: "12785"
                }
                console.log("Parameters:",JSON.stringify(param))
-              })
               
-              this.genericMethods
-               .createFolder(
-                 this.param  
-               )
+                this.genericMethods
+               .createFolder( param)
                .then((response:any)=>
                {
                  console.log("Data Extension Created...Check MC App")
                }
                )
                
-              
+              })
              
                 .catch((err: any) => {
                   console.error(
