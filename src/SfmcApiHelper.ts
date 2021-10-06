9258b2e09476cdb885992a2c0bebd2cb854384e0
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-10-06 18:14:01 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -200,7 +200,8 @@ export default class SfmcApiHelper
                {
                  console.log("Response in dataFolderCheck >>>",response)
                  console.log("Folder id in use:",response.FolderID)
-                  this.FolderID = response.FolderID 
+                  this.FolderID = response.FolderID
+                  console.log(this.FolderID) 
                   console.log("FOlderID got!!")
                })
                .catch((err:any)=>
