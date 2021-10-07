988a5fd00d6a52da6d919a099a9e50038b10c10e
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-10-07 17:06:51 +0530

    bodySoapData

--- src/Generic-method/src/index.ts
+++ src/Generic-method/src/index.ts
@@ -752,6 +752,7 @@ export default class mcGenericMethods {
         );
         
             let bodySoapData  = '';
+            console.log("BodySoapData:",bodySoapData)
             let DCmsg =
               '<?xml version="1.0" encoding="UTF-8"?>' +
               '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">' +
@@ -812,7 +813,8 @@ export default class mcGenericMethods {
                   "                        <IsPrimaryKey>"+jsonArr[i].isKey+"</IsPrimaryKey>" +
                   "                    </Field>"
                 }
-              }              
+              }       
+
               "                </Fields>" +
               "            </Objects>" +
               "        </CreateRequest>" +
