50a11be530bf4b3043d5b080f6b90fbb8424e204
HariRaj01 <hariraj01.pashtek@gmail.com>
2021-10-08 16:18:37 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -118,6 +118,7 @@ export default class SfmcApiHelper
         this.oauthAccessToken = res.data.oauthAccessToken
         console.log("tokken tokken>>",this.refreshToken); 
         console.log("AccessToken Method  library", res);
+        result.status(200).send(res)
         
         if (res.data.refresh_token) {
           console.log(
